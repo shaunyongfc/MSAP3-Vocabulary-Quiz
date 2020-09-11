a8c378d47750d3d6361fc20b8b0ae4310b45929c
Shaun <yongfongchuan@gmail.com>
2020-09-11 17:04:11 +1000

    launch in heroku

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True, port=5000)
